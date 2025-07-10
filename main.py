5371b637055f0d6e1b6f93a8c830affd23c7319d
noahsealy <noah.sealy8@gmail.com>
2025-07-10 18:51:01 -0300

    chunk working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfReader
 
-from chunker import chunk
+from chunker import Chunker
 
 def main():
     reader = PdfReader('concurrency_and_threads.pdf')
@@ -9,6 +9,10 @@ def main():
     text = page.extract_text()
     print(number_of_pages)
     print(text)
+
+    c = Chunker(chunk_size=500, overlap=50)
+    chunks = c.chunk(text)
+    print(chunks)
 
 if __name__ == '__main__':
     main()
